4cfe3310174b5740ef2378974cdf54dd6e30c755
vsyx <vsyxxio@gmail.com>
2022-02-02 20:51:26 +0200

    Init

---
+++ src/main/java/com/github/vsyx/premiumcalculator/PremiumCalculator.java
@@ -0,0 +1,9 @@
+package com.github.vsyx.premiumcalculator;
+
+public class PremiumCalculator
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/github/vsyx/premiumcalculator/PremiumCalculatorTest.java
@@ -0,0 +1,14 @@
+package com.github.vsyx.premiumcalculator;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class PremiumCalculatorTest
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
