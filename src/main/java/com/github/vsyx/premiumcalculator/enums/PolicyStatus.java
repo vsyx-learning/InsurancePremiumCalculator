f645d15fd4d6da8c44511862e64729469351e9a2
vsyx <vsyxxio@gmail.com>
2022-02-02 20:54:05 +0200

    Add enums

---
+++ src/main/java/com/github/vsyx/premiumcalculator/enums/PolicyStatus.java
@@ -0,0 +1,6 @@
+package com.github.vsyx.premiumcalculator.enums;
+
+public enum PolicyStatus {
+    REGISTERED,
+    APPROVED
+}
---
+++ src/main/java/com/github/vsyx/premiumcalculator/enums/RiskType.java
@@ -0,0 +1,6 @@
+package com.github.vsyx.premiumcalculator.enums;
+
+public enum RiskType {
+    FIRE,
+    THEFT
+}
